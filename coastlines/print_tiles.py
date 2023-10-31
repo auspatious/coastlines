36cc1a2cc9fb2f4193c632f23d25c42f6244b3d8
Alex Leith <alexgleith@gmail.com>
2023-10-31 13:01:00 +1100

    Don't print

--- coastlines/print_tiles.py
+++ coastlines/print_tiles.py
@@ -18,7 +18,6 @@ def read_tiles_subset_string(tiles_subset: str) -> list:
 @click.option("--limit", type=int, default=None, required=False)
 def cli(config_file: str, tiles_subset: str, limit: Optional[int]) -> None:
     config = load_config(config_file)
-    print(f"Loaded config from {config_file}")
     tiles = load_json(config["Input files"]["grid_path"])
 
     try:
